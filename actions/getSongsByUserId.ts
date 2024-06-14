d82c40a1d0586d7354d133b6ff7386aef677bee6
David-gmi <david.guerrero.medel@gmail.com>
2024-06-13 23:21:53 -0600

    Actualización

--- actions/getSongsByUserId.ts
+++ actions/getSongsByUserId.ts
@@ -8,17 +8,17 @@ const getSongsByUserId = async (): Promise<Song[]> => {
     cookies: cookies
   });
 
-  const { data: sessionData, error: sessionError } = await supabase.auth.getSession();
+  const user = supabase.auth.getUser();
 
-  if (sessionError) {
-    console.log(sessionError.message);
+  if (!user) {
+    console.log("No user session available.");
     return [];
   }
 
   const { data, error } = await supabase
     .from('songs')
     .select('*')
-    .eq('user_id', sessionData.session?.user.id)
+    .eq('user_id', user.id)
     .order('created_at', { ascending: false })
 
   if (error) {
