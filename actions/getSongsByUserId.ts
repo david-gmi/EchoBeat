f53c6e07226b9f5742e4a4a6e0eb97efbb26b1f7
david-gmi <130802746+david-gmi@users.noreply.github.com>
2024-06-13 23:57:04 -0600

    Update getSongsByUserId.ts

--- actions/getSongsByUserId.ts
+++ actions/getSongsByUserId.ts
@@ -4,40 +4,28 @@ import { cookies } from "next/headers";
 import { Song } from "@/types";
 
 const getSongsByUserId = async (): Promise<Song[]> => {
-  try {
-    const supabase = createServerComponentClient({
-      cookies: cookies
-    });
+  const supabase = createServerComponentClient({
+    cookies: cookies
+  });
 
-    // Obtener el usuario de manera asíncrona
-    const { user, error: userError } = await supabase.auth.getUser();
+  const { data: sessionData, error: sessionError } = await supabase.auth.getSession();
 
-    // Manejar errores al obtener el usuario
-    if (userError) {
-      throw new Error(`Error al obtener el usuario: ${userError.message}`);
-    }
-
-    if (!user) {
-      console.log("No hay sesión de usuario disponible.");
-      return [];
-    }
-
-    // Obtener canciones del usuario usando su ID
-    const { data: songsData, error: songsError } = await supabase
-      .from('songs')
-      .select('*')
-      .eq('user_id', user.id)
-      .order('created_at', { ascending: false });
-
-    // Manejar errores al obtener las canciones
-    if (songsError) {
-      throw new Error(`Error al obtener canciones: ${songsError.message}`);
-    }
-
-    // Retornar los datos de canciones, asegurándose que sea del tipo Song[]
-    return songsData || [];
-  } catch (error) {
-    console.error(error);
-    return []; // Retornar un arreglo vacío o manejar el error según tu lógica de aplicación
+  if (sessionError) {
+    console.log(sessionError.message);
+    return [];
   }
+
+  const { data, error } = await supabase
+    .from('songs')
+    .select('*')
+    .eq('user_id', sessionData.session?.user.id)
+    .order('created_at', { ascending: false })
+
+  if (error) {
+    console.log(error.message);
+  }
+
+  return (data as any) || [];
 };
+
+export default getSongsByUserId;
