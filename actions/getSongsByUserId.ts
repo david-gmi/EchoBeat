e38e3f689a3bc88afa808738b2d1c0bd89f9ca14
david-gmi <130802746+david-gmi@users.noreply.github.com>
2024-06-13 23:44:55 -0600

    Update getSongsByUserId.ts

--- actions/getSongsByUserId.ts
+++ actions/getSongsByUserId.ts
@@ -4,37 +4,40 @@ import { cookies } from "next/headers";
 import { Song } from "@/types";
 
 const getSongsByUserId = async (): Promise<Song[]> => {
-  const supabase = createServerComponentClient({
-    cookies: cookies
-  });
+  try {
+    const supabase = createServerComponentClient({
+      cookies: cookies
+    });
 
-  // Obtener el usuario de manera asíncrona
-  const { user, error: userError } = await supabase.auth.getUser();
+    // Obtener el usuario de manera asíncrona
+    const { user, error: userError } = await supabase.auth.getUser();
 
-  // Manejar errores al obtener el usuario
-  if (userError) {
-    console.error("Error al obtener el usuario:", userError.message);
-    return [];
+    // Manejar errores al obtener el usuario
+    if (userError) {
+      throw new Error(`Error al obtener el usuario: ${userError.message}`);
+    }
+
+    if (!user) {
+      console.log("No hay sesión de usuario disponible.");
+      return [];
+    }
+
+    // Obtener canciones del usuario usando su ID
+    const { data: songsData, error: songsError } = await supabase
+      .from('songs')
+      .select('*')
+      .eq('user_id', user.id)
+      .order('created_at', { ascending: false });
+
+    // Manejar errores al obtener las canciones
+    if (songsError) {
+      throw new Error(`Error al obtener canciones: ${songsError.message}`);
+    }
+
+    // Retornar los datos de canciones, asegurándose que sea del tipo Song[]
+    return songsData || [];
+  } catch (error) {
+    console.error(error);
+    return []; // Retornar un arreglo vacío o manejar el error según tu lógica de aplicación
   }
-
-  if (!user) {
-    console.log("No hay sesión de usuario disponible.");
-    return [];
-  }
-
-  // Obtener canciones del usuario usando su ID
-  const { data: songsData, error: songsError } = await supabase
-    .from('songs')
-    .select('*')
-    .eq('user_id', user.id)  // Acceder a user.id correctamente
-    .order('created_at', { ascending: false });
-
-  // Manejar errores al obtener las canciones
-  if (songsError) {
-    console.error("Error al obtener canciones:", songsError.message);
-    return [];
-  }
-
-  // Retornar los datos de canciones, asegurándose que sea del tipo Song[]
-  return songsData || [];
 };
