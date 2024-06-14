28bf5d37e1f317aa5c64ac40fe573926747e6044
david-gmi <130802746+david-gmi@users.noreply.github.com>
2024-06-13 23:39:34 -0600

    getSongsByUserId.ts

--- actions/getSongsByUserId.ts
+++ actions/getSongsByUserId.ts
@@ -3,19 +3,13 @@ import { cookies } from "next/headers";
 
 import { Song } from "@/types";
 
-// Define la interfaz del usuario según tus definiciones
-interface User {
-  id: string;
-  // Otras propiedades del usuario según sea necesario
-}
-
 const getSongsByUserId = async (): Promise<Song[]> => {
   const supabase = createServerComponentClient({
     cookies: cookies
   });
 
   // Obtener el usuario de manera asíncrona
-  const { data: userData, error: userError } = await supabase.auth.getUser();
+  const { data: userResponse, error: userError } = await supabase.auth.getUser();
 
   // Manejar errores al obtener el usuario
   if (userError) {
@@ -23,18 +17,19 @@ const getSongsByUserId = async (): Promise<Song[]> => {
     return [];
   }
 
-  if (!userData || !userData.user || !userData.user.id) {
-    console.log("No hay sesión de usuario disponible o falta el ID.");
+  // Extraer el usuario de la respuesta
+  const user = userResponse?.user;
+
+  if (!user) {
+    console.log("No hay sesión de usuario disponible.");
     return [];
   }
-
-  const userId = userData.user.id;
 
   // Obtener canciones del usuario usando su ID
   const { data: songsData, error: songsError } = await supabase
     .from('songs')
     .select('*')
-    .eq('user_id', userId)
+    .eq('user_id', user.id)  // Acceder a user.id correctamente
     .order('created_at', { ascending: false });
 
   // Manejar errores al obtener las canciones
